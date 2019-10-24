4a39ac87f6d0b538927005ccd93d16c2c8cd4a78
WeustiS <willceustis@gmail.com>
2019-10-24 00:38:32 -0500

    I love regex, Quinn please help

---
+++ having_fun_with_neo4j/neo4j.py
@@ -0,0 +1,54 @@
+file = r"D:\Downloads\2019-fa.csv"
+
+import pandas as pd
+import re
+classes  = []
+
+class Class:
+    def __init__(self, sub, num, name, title, desc, hours, preqs, attrs):
+        self.subject = sub
+        self.number = num
+        if len(str(num)) != 3:
+            print(title)
+        self.name = name
+        self.title = title
+        self.desc = desc
+        hours = re.findall(r"\d\.?\d?", hours)
+        self.hours = hours
+        dict = {}
+        if ". See" in preqs:
+            dict["cross_listed"] = re.search(r"/[A-Z]{2,5} [0-9]{1,3}", preqs)
+        else:
+            index = preqs.find("Prerequisite")
+            preqs = preqs[index:]
+            dict = get_rels(preqs)
+
+        self.preqs = preqs
+        self.attr = attrs
+
+def get_rels(preqs):
+        if "OR" in preqs:
+            get_rels(preqs[:preqs.find("OR")-1])
+            get_rels(preqs[preqs.find("OR")+3:])
+        
+def main():
+    ca = pd.read_csv(file)
+    for i, r in ca.iterrows():
+        sub = r["Subject"]
+        num = r["Number"]
+        name = r["Name"]
+        title = sub + " " + str(num)
+        desc = r["Description"]
+        hours = r["Credit Hours"]
+        preqs = r["Section Info"]
+        attrs = r["Degree Attributes"]
+        c = Class(sub, num, name, title, desc, hours, preqs, attrs)
+        classes.append(c)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+# Year	Term	YearTerm	Subject	Number	Name	Description	Credit Hours	Section Info	Degree Attributes	Schedule Information
